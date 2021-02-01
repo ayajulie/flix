e45b22f5040cb6a5c848448a6e422b4952b12313
Aya Koffi <ayajulie@gmail.com>
2021-02-01 20:27:42 +0100

    set up the app

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,25 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :total_gross, :released_on, :rating)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'movies#index'
+  get 'movies', to: 'movies#index'
+  get 'movies/:id', to: 'movies#show', as: :movie
+  get 'movies/:id/edit', to: 'movies#edit', as: :edit_movie
+  patch 'movies/:id', to: 'movies#update'
+end
