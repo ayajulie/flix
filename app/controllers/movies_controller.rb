c0b101058050a0919a7bf1d77e45884c672e1370
Aya Koffi <ayajulie@gmail.com>
2021-02-08 21:14:44 +0100

    Added the option to create a new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,24 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new
+
+    respond_to do |format|
+      if @movie.save
+        flash[:notice] = 'Movie was successfully created.'
+        format.html { redirect_to(@movie) }
+
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
