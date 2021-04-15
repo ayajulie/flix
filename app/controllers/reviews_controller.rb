d5c18940142383dbcae9f6b86b0cdd73b2178723
Aya Koffi <ayajulie@gmail.com>
2021-04-15 20:00:48 +0200

    updates the show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -25,23 +25,6 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
-  # def edit
-  #   @review = Review.find(params[:id])
-  #   # @movie = @review.movie
-  # end
-
-  # def update
-  #   @review = Review.find(params[:id])
-
-  #   if @review.update(review_params)
-  #     flash[:success] = 'Object was successfully updated'
-  #     redirect_to @review
-  #   else
-  #     flash[:error] = 'Something went wrong'
-  #     render :edit
-  #   end
-  # end
-
   private
 
   def review_params
