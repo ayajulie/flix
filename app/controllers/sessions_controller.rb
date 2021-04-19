a901ce62f431e4891aa63d0b8301aea48ab08f9f
Aya Koffi <ayajulie@gmail.com>
2021-04-19 11:04:25 +0200

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    params[:email]
-    params[:password]
+    user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+    if user && user.authenticate(params[:password]) # check if nil and correct password
+      session[:user_id] = user.id # put user id in session
+      redirect_to user, notice: "Welcome back #{user.name}"
+    else
+      flash.now[:alert] = 'Invalid credentials' # .now displays the alert before rendering
+      render :new
+    end
   end
 
   def destroy; end
