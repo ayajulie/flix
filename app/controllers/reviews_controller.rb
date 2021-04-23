ceb2da3f9f491cc249c2c1a2d30298bd1bf94e59
Aya Koffi <ayajulie@gmail.com>
2021-04-23 21:28:40 +0200

    updates the review controlller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -34,7 +34,7 @@ class ReviewsController < ApplicationController
   end
 
   def set_movie
-    @movie = Movie.find_by!(slug: params[:id])
+    @movie = Movie.find_by!(slug: params[:movie_id])
   end
   # def set_movie
   #   @movie = Movie.find(params[:movie_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   resources :genres
   root 'movies#index'
   get 'movies/filter/:filter', to: 'movies#index', as: :filtered_movies
+
   resources :movies do
     resources :reviews
     resources :favorites, only: %i[create destroy]
   end
+
+  resource :session, only: %i[new create destroy]
+
   resources :users
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
-  resource :session, only: %i[new create destroy]
 end
