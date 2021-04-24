a3984e960d20162abd4d8a1753a3c72e5fdff192
Aya Koffi <ayajulie@gmail.com>
2021-04-24 10:35:04 +0200

    Declares the movie association

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,6 +7,7 @@ class Movie < ApplicationRecord
   has_many :fans, through: :favorites, source: :user # transforms users to an alias "fans"
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
+  has_one_attached :main_image
 
   RATINGS = %w[G PG PG-13 R NC-17].freeze
 
@@ -14,10 +15,10 @@ class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
-  validates :image_file_name, format: {
-    with: /\w+\.(jpg|png)\z/i,
-    message: 'must be a JPG or PNG image'
-  }
+  # validates :image_file_name, format: {
+  #   with: /\w+\.(jpg|png)\z/i,
+  #   message: 'must be a JPG or PNG image'
+  # }
   validates :rating, inclusion: { in: RATINGS }
 
   scope :released, -> { where('released_on < ?', Time.now).order(released_on: :desc) }
